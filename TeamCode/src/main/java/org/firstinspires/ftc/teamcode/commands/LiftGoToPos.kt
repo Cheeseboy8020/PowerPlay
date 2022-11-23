16c362dc119d3e171b4dbc0a16fe84a1b052c0b3
cheeseboy <aneeshjosyula@gmail.com>
2022-11-23 16:35:21 -0500

    made coeffs configurable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LiftGoToPos.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LiftGoToPos.kt
@@ -1,13 +1,15 @@
 package org.firstinspires.ftc.teamcode.commands
 
+import com.acmerobotics.dashboard.config.Config
 import com.acmerobotics.roadrunner.control.PIDCoefficients
 import com.acmerobotics.roadrunner.control.PIDFController
 import com.arcrobotics.ftclib.command.CommandBase
 import org.firstinspires.ftc.teamcode.subsystems.Lift
 import kotlin.math.abs
 
+@Config
 class LiftGoToPos(private val lift: Lift, private val pos: Lift.Positions) : CommandBase() {
-    val coeffs = PIDCoefficients(0.0, 0.0, 0.0)
+    @JvmField val coeffs = PIDCoefficients(0.0, 0.0, 0.0)
     val tolerance = 10.0
     val liftController = PIDFController(coeffs, 0.0, 0.0, 0.1)
 
