342a7fbf6d3bc1b76e20e0d5c2db4cd91b0443ef
cheeseboy <aneeshjosyula@gmail.com>
2023-01-15 11:49:07 -0500

    fixed errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.kt
@@ -5,7 +5,6 @@ import com.arcrobotics.ftclib.command.SubsystemBase
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import org.firstinspires.ftc.teamcode.autonomous.right.Right
 
 class Intake(hardwareMap: HardwareMap, val telemetry: Telemetry) : SubsystemBase() {
     var leftPinch: Servo
